2838f83e8fbac127ed6ddd823badf3c50fec63bc
laszlo-v <109346622+laszlo-v@users.noreply.github.com>
2023-08-01 11:33:04 +0100

    use strict, IIFE

--- app.js
+++ app.js
@@ -1,76 +1,79 @@
-const numbers = document.querySelector(".numbers");
-const sum = document.querySelector(".sum");
-const results = document.querySelector(".result");
-const get = document.querySelector(".get");
-const reset = document.querySelector(".reset");
+"use strict";
+(() => {
+  const numbers = document.querySelector(".numbers");
+  const sum = document.querySelector(".sum");
+  const results = document.querySelector(".result");
+  const get = document.querySelector(".get");
+  const reset = document.querySelector(".reset");
 
-get.addEventListener("click", () => {
-  console.log(numbers.value);
-  const numbersString = numbers.value.split(",");
-  const numbersArray = [];
-  for (let number of numbersString) {
-    numbersArray.push(+number);
-  }
-
-  let targetNumber = +sum.value;
-  const findNumber = (
-    arr,
-    target,
-    startIndex,
-    currentSum,
-    currentCombination,
-    result
-  ) => {
-    const tolerance = 0.0001;
-
-    if (Math.abs(currentSum - target) < tolerance) {
-      result.push([...currentCombination]);
-      return;
+  get.addEventListener("click", () => {
+    console.log(numbers.value);
+    const numbersString = numbers.value.split(",");
+    const numbersArray = [];
+    for (let number of numbersString) {
+      numbersArray.push(+number);
     }
 
-    for (let i = startIndex; i < arr.length; i++) {
-      // Skip duplicate elements
-      if (i > startIndex && arr[i] === arr[i - 1]) {
-        continue;
+    let targetNumber = +sum.value;
+    const findNumber = (
+      arr,
+      target,
+      startIndex,
+      currentSum,
+      currentCombination,
+      result
+    ) => {
+      const tolerance = 0.0001;
+
+      if (Math.abs(currentSum - target) < tolerance) {
+        result.push([...currentCombination]);
+        return;
       }
 
-      currentCombination.push(arr[i]);
-      findNumber(
-        arr,
-        target,
-        i + 1,
-        currentSum + arr[i],
-        currentCombination,
-        result
-      );
-      currentCombination.pop();
-    }
-  };
+      for (let i = startIndex; i < arr.length; i++) {
+        // Skip duplicate elements
+        if (i > startIndex && arr[i] === arr[i - 1]) {
+          continue;
+        }
 
-  const findAllCombinations = (arr, target) => {
-    arr.sort((a, b) => a - b); // Sort the input array for handling duplicates
-    const result = [];
-    findNumber(arr, target, 0, 0, [], result);
-    return result;
-  };
+        currentCombination.push(arr[i]);
+        findNumber(
+          arr,
+          target,
+          i + 1,
+          currentSum + arr[i],
+          currentCombination,
+          result
+        );
+        currentCombination.pop();
+      }
+    };
 
-  const result = findAllCombinations(numbersArray, targetNumber);
-  console.log("Combinations that add up to the target number:");
-  console.log(result);
+    const findAllCombinations = (arr, target) => {
+      arr.sort((a, b) => a - b); // Sort the input array for handling duplicates
+      const result = [];
+      findNumber(arr, target, 0, 0, [], result);
+      return result;
+    };
 
-  result.forEach((e, i) => {
-    paragraph = document.createElement("p");
-    paragraph.textContent = `Combination ${i + 1} is: ${e}`;
-    if (!results.hasChildNodes(paragraph)) {
-      results.appendChild(paragraph);
+    const result = findAllCombinations(numbersArray, targetNumber);
+    console.log("Combinations that add up to the target number:");
+    //console.log(result);
+
+    result.forEach((e, i) => {
+      paragraph = document.createElement("p");
+      paragraph.textContent = `Combination ${i + 1} is: ${e}`;
+      if (!results.hasChildNodes(paragraph)) {
+        results.appendChild(paragraph);
+      }
+    });
+  });
+
+  reset.addEventListener("click", () => {
+    numbers.value = "";
+    sum.value = "";
+    if (results.hasChildNodes(paragraph)) {
+      results.removeChild(paragraph);
     }
   });
-});
-
-reset.addEventListener("click", () => {
-  numbers.value = "";
-  sum.value = "";
-  if (results.hasChildNodes(paragraph)) {
-    results.removeChild(paragraph);
-  }
-});
+})();
